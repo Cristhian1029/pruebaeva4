5dc82246e0eeeb41de4ee6d5d16f777f5f89cb72
Cristhian1029 <144628937+Cristhian1029@users.noreply.github.com>
2024-11-25 15:35:10 -0300

    cambio agregando una parte react pero no estando en uso por ahora simplemente agregado de archivo template para uso

--- proyects/urls.py
+++ proyects/urls.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
+from django.urls import path
 from .api import ProjectViewSet
-
+from .views import home 
 router = routers.DefaultRouter()
-
 router.register('api/projects', ProjectViewSet, 'projects')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('', home, name='home'),  
+]
--- pruebasimplecrud/settings.py
+++ pruebasimplecrud/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'pruebasimplecrud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
